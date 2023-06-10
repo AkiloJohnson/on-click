59c1e2d074bf9d00e47ee4cd04a72f7d2f9bf9a5
Bolaji <bolajijohnson0007@gmail.com>
2023-06-10 15:31:45 +0100

    frist commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// const btn_click = document.getElementById('btn-click')
+// btn_click.addEventListener('click',()=>{
+//     const myform = document.getElementById('myForm')
+//     myform.style.backgroundColor = 'black';
+// })
+
+
+const btn_click = document.getElementById('btn-click')
+btn_click.addEventListener('click',()=>{
+    const myform = document.getElementById('mytext')
+    myform.style.fontSize = '50px';
+})
+
+// btn_click.addEventListener('click', clickbtn);
+
+// function clickbtn(e){
+//     e.preventDefault();
+
+//     const myform = document.getElementById('mytext')
+//     myform.style.fontSize = '50px';
+// }
